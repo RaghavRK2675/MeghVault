a8166702799fa498c9e4ef817269855c36c2b9ae
Raghav Kumar <raghavrk2675@gmail.com>
2024-11-17 19:17:15 +0530

    second commit

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const userRouter = require('./routes/user.routes');
 const app = express();
+const dotenv = require('dotenv');
+
+dotenv.config();
 
 app.set('view engine', 'ejs');
 
